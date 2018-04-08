e55669708ad80500caa490883fe4b2f9d0bf738e
Mikael Olausson <mikaeo@usit.uio.no>
2018-04-08 22:14:57 +0200

    MOB-1675: Background-color of radio-elements should be white

--- src/svg-icons/RadioChecked.js
+++ src/svg-icons/RadioChecked.js
@@ -7,7 +7,7 @@ const RadioChecked = (p: { color?: string }) => (
   <Svg height="22" width="22" viewBox="0 0 22 22">
     <G stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
       <G id="Radio" stroke={p.color || colors.primary} strokeWidth="2">
-        <Circle cx="11" cy="11" r="10" />
+        <Circle fill="white" cx="11" cy="11" r="10" />
       </G>
       <Circle fill={p.color || colors.primary} cx="11" cy="11" r="5" />
     </G>
