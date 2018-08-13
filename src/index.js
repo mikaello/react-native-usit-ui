c56f071238d5128cd810ba2c270053cf7f16bb2a
Tom Fredrik Christoffersen <tomfch@usit.uio.no>
2018-08-13 12:20:25 +0200

    inkluder CircularSlider i src/index

--- src/index.js
+++ src/index.js
@@ -14,3 +14,4 @@ export { default as CircularTimeSlider } from './CircularTimeSlider';
 export { default as GradientBackground } from './GradientBackground';
 export { default as ProgressCircle } from './ProgressCircle';
 export { default as ProgressBar } from './ProgressBar';
+export { default as CircularSlider } from './CircularSlider';
